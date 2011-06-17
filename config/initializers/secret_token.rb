a6526dc22897cdc29cd6a52b5e2045362dcf11f9
Michael Kuhinica <taverneiro@gmail.com>
2011-06-17 16:12:08 -0300

    rails 3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WgCapistrano::Application.config.secret_token = '72113ed5424b9a4fdca4c4a163d7863499f8fe8091b753b502c4d108494aa95a7daca179340234c5d850fc01592c2bd0f39d6cc55f68438ea708e84877545912'
